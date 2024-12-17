bf05c3dcdab78027c011abdad35801260f383cfb
Leo Bianco <leonardo@rhl.com.br>
2024-12-17 11:15:25 +0100

    added formatting_prompts_func that I somehow forgot to copy from the Colab

--- writer_sft.py
+++ writer_sft.py
@@ -15,6 +15,33 @@ from data import *
 from utils import ScriptArguments, CustomLoraConfig
 
 
+def formatting_prompts_func(entry):
+  """Formatting function for training examples passed to SFTTrainer."""
+
+  template = (
+    "Translate a text originally written in {src_lang} into {tgt_lang}. "
+    "Generate only the translated text, and nothing else."
+    "\nOriginal text: {src_text}"
+  )
+
+  output_texts = []
+
+  for i in range(len(entry['src_text'])):
+    formatted_prompt = template.format(
+        src_lang=entry["src_lang"][i],
+        tgt_lang=entry["tgt_lang"][i],
+        src_text=entry["src_text"][i],
+    )
+
+    text = (
+        f"{formatted_prompt}\nTranslated text: {entry['completion'][i]}"
+    )
+
+    output_texts.append(text)
+
+  return output_texts
+
+
 def main():
 
   #########
