104c4ddd00313002e3732205c905802fa93eb5a2
Leonardo Martins Bianco <leonardo@rhl.com.br>
2025-02-05 18:10:44 +0100

    minor changes to comment on reward model files

--- reward_model.sh
+++ reward_model.sh
@@ -5,12 +5,13 @@ if [ $SHLVL -gt 2 ]; then
   :
 else
   # If single-run, set variables here.
+  DATASET="$1"
   DEEPSPEED_CONFIG="./deepspeed_config.yaml"
   SEED=130104
   NUM_TRAIN_EPOCHS=3
   LEARNING_RATE=1e-3
   LORA_RANK=8
-  RUN_IDENTIFIER="leobianco/HALOMI_RM_seed_${SEED}_epochs_${NUM_TRAIN_EPOCHS}_lr_${LEARNING_RATE}_lora_${LORA_RANK}"
+  RUN_IDENTIFIER="leobianco/${DATASET}_RM_seed_${SEED}_epochs_${NUM_TRAIN_EPOCHS}_lr_${LEARNING_RATE}_lora_${LORA_RANK}"
 fi
 
 accelerate launch \
@@ -21,11 +22,11 @@ accelerate launch \
   --report_to "wandb" \
   --run_name "$RUN_IDENTIFIER" \
   --logging_steps 5 \
-  --output_dir "./checkpoints/halomi/reward_model/${RUN_IDENTIFIER}" \
+  --output_dir "./checkpoints/${DATASET}/reward_model/${RUN_IDENTIFIER}" \
   --overwrite_output_dir True \
   --push_to_hub True \
   --hub_model_id "$RUN_IDENTIFIER" \
-  --dataset_name "leobianco/rm_halomi_processed" \
+  --dataset_name "leobianco/rm_${DATASET}_processed" \
   --model_identifier "google/gemma-2-2b-it" \
   --do_train True \
   --bf16 True \
