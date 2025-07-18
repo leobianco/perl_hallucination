7694eec65f12c27b2cc8f4f89ff9475fa2009f2f
Leonardo Martins Bianco <leonardo@rhl.com.br>
2025-07-18 11:37:52 +0200

    fix - debugging

--- data/npov_task_processor.py
+++ data/npov_task_processor.py
@@ -668,7 +668,7 @@ class NPOVTaskProcessor(BaseTaskProcessor):
             return arguments
 
         # Load new perspectives from local CSV
-        new_perspectives = pd.read_csv("npov_new_perspectives.csv")
+        new_perspectives = pd.read_csv("./data/npov_new_perspectives.csv")
 
         for topic in topics:
             user_query = data.filter(lambda x: x["topic"] == topic)[
