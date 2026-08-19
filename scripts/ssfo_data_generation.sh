345b7cf2b697f2a10831cffc77ea0e850c88b779
Leonardo Martins Bianco <leobianco@google.com>
2026-08-19 09:37:01 +0000

    Add USE_BASE_MODEL_FOR_REJECTED flag to ssfo_data_generation.sh

--- scripts/ssfo_data_generation.sh
+++ scripts/ssfo_data_generation.sh
@@ -14,6 +14,7 @@ MAX_NEW_TOKENS=250
 BATCH_SIZE=16
 MAX_SAMPLES=""  # Set number of samples here directly (e.g. 500), or leave empty for full data
 USE_GROUND_TRUTH_CHOSEN=False
+USE_BASE_MODEL_FOR_REJECTED=True
 
 # Parameters derived from above
 TASK_NAME="$1"
@@ -54,6 +55,7 @@ CMD_ARGS=(
   --max_new_tokens "$MAX_NEW_TOKENS"
   --batch_size "$BATCH_SIZE"
   --use_ground_truth_chosen "$USE_GROUND_TRUTH_CHOSEN"
+  --use_base_model_for_rejected "$USE_BASE_MODEL_FOR_REJECTED"
   --push_to_hub True
 )
 
